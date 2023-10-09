36e0a29e192d19f8c59cea2f15c610d29f5afc57
Makarikhin Pavel <Makarikhin.p.a@gmail.com>
2023-10-09 12:41:52 +0300

    Исправлены несколько багов, удалена диагностика и сравнение с последовательной версией алгоритма.

--- file_hashing_sha256/file_hashing_sha256/Program.cs
+++ file_hashing_sha256/file_hashing_sha256/Program.cs
@@ -1,18 +1,5 @@
-﻿using System.Diagnostics;
-using System.Text;
+﻿using System.Text;
 using file_hashing_sha256;
-
-var sw = new Stopwatch();
-
-
-var test1 =
-    @"D:\GitHub\file_hashing_sha256\file_hashing_sha256\file_hashing_sha256_tests\tests_data\simple_text_file.txt";
-var test2 =
-    @"D:\GitHub\file_hashing_sha256\file_hashing_sha256\file_hashing_sha256_tests\tests_data\tolstoy.txt";
-var test3 =
-    @"D:\GitHub\file_hashing_sha256\file_hashing_sha256\file_hashing_sha256_tests\tests_data\simple_binary_file.txt";
-var test4 =
-    @"D:\GitHub\file_hashing_sha256\file_hashing_sha256\file_hashing_sha256_tests\tests_data\dlc1.rpkg";
 
 var fileName = Console.ReadLine();
 int segmentsCount;
@@ -34,7 +21,6 @@ if (fileInfo.Length < segmentsCount)
     throw new ArgumentException("Invalid number of segments! Can't split file!");
 }
 
-var SHA256SH = new SHA256SegmentHashing(fileInfo.Length, segmentsCount);
 var SHA256SHP = new SHA256SegmentHashingParallel(fileInfo.Length, segmentsCount);
 var cts = new CancellationTokenSource();
 var token = cts.Token;
@@ -44,7 +30,7 @@ string HashToString(byte[]? hash)
 {
     if (hash == null)
     {
-        throw new NullReferenceException();
+        throw new ArgumentNullException();
     }
     
     var sb = new StringBuilder();
@@ -75,38 +61,32 @@ using (var stream = fileInfo.Open(FileMode.Open))
 {
     using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
     {
-        sw.Start();
-        
         Task.Factory.StartNew(() =>
         {
             Console.WriteLine("Press 'q' to abort calculation...");
             if (Console.ReadKey().KeyChar != 'q') return;
             cts.Cancel();
-            Console.Clear();
+
         });
         
         var hashesParallel = SHA256SHP.CalculateSHA256HashesParallel(reader, token);
         
-        sw.Stop();
-        Console.WriteLine($"Time: {sw.ElapsedMilliseconds}");
-        
         try
         {
-            PrintSegmentsHashes(hashesParallel);
+            if (!cts.IsCancellationRequested)
+            {
+                PrintSegmentsHashes(hashesParallel);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Calculations aborted..");
+            }
         }
-        catch (NullReferenceException)
+        catch (ArgumentNullException)
         {
             Console.Write("Invalid operation!");
         }
-
-
-        reader.BaseStream.Seek(0, SeekOrigin.Begin);
-        sw.Reset();
-        sw.Start();
-        var hashes = SHA256SH.CalculateSHA256Hashes(reader);
-        sw.Stop();
-        Console.WriteLine($"Time: {sw.ElapsedMilliseconds}");
-        PrintSegmentsHashes(hashes);
     }
 }
 
